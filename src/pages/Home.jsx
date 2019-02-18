0f1bfa0f01ca0eb5086c6cbcc9c56525a6fdd99c
dtwilliams10 <david.williams110913@gmail.com>
2019-02-17 23:17:56 -0500

    Added material UI and changed to begin rendering data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 
 class Home extends Component {
-
-};
+render() {
+    return (
+        <div>
+            <p>This is a test</p>
+        </div>
+        );
+    }
+}
 
 export default Home;
