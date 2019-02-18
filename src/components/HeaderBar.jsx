999ff92c90e30b08cc910b19194e2ed09343e7a0
dtwilliams10 <david.williams110913@gmail.com>
2019-02-18 09:05:23 -0500

    Stubbed out new components

---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HeaderBar extends Component {
+    render() {
+        return(
+            <div>
+                <p>Header Bar</p>
+            </div>
+        );
+    }
+}
+
+export default HeaderBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import HeaderBar from '../components/HeaderBar';
 
 class Home extends Component {
 render() {
     return (
         <div>
+            <HeaderBar/>
             <p>This is a test</p>
         </div>
         );
